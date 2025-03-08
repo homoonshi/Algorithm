029439e90edd1a04c6a90263566356817136b775
MoonHeeKim <homoonshi@gmail.com>
2025-03-09 07:49:07 +0900

    [Gold IV] Title: 오렌지 출하, Time: 1372 ms, Memory: 39740 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/11985. 오렌지 출하/오렌지 출하.java
+++ 백준/Gold/11985. 오렌지 출하/오렌지 출하.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -15,25 +14,31 @@ public class Main {
         int M = Integer.parseInt(input[1]);
         int K = Integer.parseInt(input[2]);
 
-        long[][] dp = new long[N+1][M+1];
-        long[][] min = new long[N+1][M+1];
-        long[][] max = new long[N+1][M+1];
+        int R = M+1;
+
+        long[][] dp = new long[R][M+1];
+        int[][] min = new int[R][M+1];
+        int[][] max = new int[R][M+1];
 
         int[] orange = new int[N+1];
 
-        for(int i=1; i<=N; i++){
+        for(int i=0; i<R; i++){
             Arrays.fill(dp[i], Long.MAX_VALUE);
-            Arrays.fill(min[i], Long.MAX_VALUE);
-            Arrays.fill(max[i], Long.MIN_VALUE);
+            Arrays.fill(min[i], Integer.MAX_VALUE);
+            Arrays.fill(max[i], Integer.MIN_VALUE);
         }
 
         for(int i=1; i<=N; i++){
             orange[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.fill(dp[0], 0);
-
         for(int i=1; i<=N; i++){
+
+            if(i>R) {
+                Arrays.fill(dp[i % R], Long.MAX_VALUE);
+                Arrays.fill(min[i % R], Integer.MAX_VALUE);
+                Arrays.fill(max[i % R], Integer.MIN_VALUE);
+            }
 
             for(int j=1; j<=M; j++){
 
@@ -42,21 +47,24 @@ public class Main {
                 }
 
                 if(j==1){
+                    if(i==1){
+                        dp[i%R][j] = 0;
+                    }
                     for(int k=1; k<=M; k++){
-                        dp[i][j] = Math.min(dp[i][j], dp[i-1][k]);
+                        dp[i%R][j] = Math.min(dp[i%R][j], dp[(i-1)%R][k]);
                     }
                 }
 
                 if(j!=1){
-                    min[i][j] = min[i-1][j-1];
-                    max[i][j] = max[i-1][j-1];
-                    dp[i][j] = dp[i-j+1][1] - (K);
+                    min[i%R][j] = min[(i-1)%R][j-1];
+                    max[i%R][j] = max[(i-1)%R][j-1];
+                    dp[i%R][j] = dp[(i-j+1)%R][1] - (K);
                 }
 
-                min[i][j] = Math.min(min[i][j], orange[i]);
-                max[i][j] = Math.max(max[i][j], orange[i]);
+                min[i%R][j] = Math.min(min[i%R][j], orange[i]);
+                max[i%R][j] = Math.max(max[i%R][j], orange[i]);
 
-                dp[i][j] += K + j * (max[i][j] - min[i][j]);
+                dp[i%R][j] += K + (long) j * (max[i%R][j] - min[i%R][j]);
 
             }
 
@@ -65,7 +73,7 @@ public class Main {
         long result = Long.MAX_VALUE;
 
         for(int i=1; i<=M; i++){
-            result = Math.min(result, dp[N][i]);
+            result = Math.min(result, dp[N%R][i]);
         }
 
         bw.write(result+"");
