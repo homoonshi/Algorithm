66bc74a90cbbcabab6e0a356858db9263b69b2cc
MoonHeeKim <homoonshi@gmail.com>
2025-06-11 03:18:23 +0900

    [Gold IV] Title: N-Queen, Time: 8120 ms, Memory: 18292 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,112 +1,81 @@
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static boolean[] height;
-    static int[] ban;
-    static int[][][] map;
-    static int N, res;
-    static boolean[] right;
-    static boolean[] left;
-
-    public static void main(String[] args) throws IOException{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
-        res = 0;
-
-        height = new boolean[N];
-        ban = new int[N];
-
-        map = new int[N][N][3];
-        left = new boolean[2*N];
-        right = new boolean[2*N];
-
-        initLeft();
-        initRight();
-
-        recursion(0);
-
-        bw.write(res+"");
-        bw.flush();
-
-    }
-
-    public static void recursion(int y){
-
-        if(y==N){
-            res++;
-            return;
-        }
-
-        for(int i=0; i<N; i++){
-            if(height[i] || right[map[i][y][2]] || left[map[i][y][1]]) continue;
-            height[i] = true;
-            right[map[i][y][2]] = true;
-            left[map[i][y][1]] = true;
-            recursion(y+1);
-            right[map[i][y][2]] = false;
-            left[map[i][y][1]] = false;
-            height[i] = false;
-        }
-
-    }
-
-    public static void initLeft(){
-        int x = N-1;
-        int y = 0;
-        for(int index=1; index<2*N; index++){
-            int nx = x;
-            int ny = y;
-            map[nx][ny][1] = index;
-            while(true){
-                nx--;
-                ny--;
-                if(!isIn(nx, ny)){
-                    break;
-                }
-                map[nx][ny][1] = index;
-            }
-            if(y!=N-1){
-                y++;
-            }else{
-                x--;
-            }
-        }
-    }
-
-    public static void initRight(){
-        int x = N-1;
-        int y = N-1;
-        for(int index=1; index<2*N; index++){
-            int nx = x;
-            int ny = y;
-            map[nx][ny][2] = index;
-            while(true){
-                nx--;
-                ny++;
-                if(!isIn(nx, ny)){
-                    break;
-                }
-                map[nx][ny][2] = index;
-            }
-            if(y!=0){
-                y--;
-            }else{
-                x--;
-            }
-        }
-    }
-
-    public static boolean isIn(int x, int y){
-        if(x>=0 && x<N && y>=0 && y<N){
-            return true;
-        }
-        return false;
-    }
+	
+	static int num=0;
+	static int N;
+	static int until[][];
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N=Integer.parseInt(br.readLine());
+		num=0;
+		until=new int[N][2];
+		
+		if(N==1) {
+			System.out.println(1);
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			recursion(i,0,0);
+		}
+		
+		System.out.print(num);
+	}
+	
+	static int go[][]= {{0,1},{1,1},{-1,1}};
+	
+	private static void recursion(int i,int j,int count) {
+		
+		if(j==0) {
+			until[count][0]=i;
+			until[count][1]=j;
+			for(int m=0;m<N;m++) {
+				recursion(m,j+1,count+1);
+			}
+			return;
+		}
+		
+		int tx,ty;
+		int dis;
+		
+		for(int m=0;m<count;m++) {
+			
+			dis=j-m;
+			
+			for(int n=0;n<3;n++) {
+				
+				tx=until[m][0]+go[n][0]*dis;
+				ty=until[m][1]+go[n][1]*dis;
+				
+				if(tx==i&&ty==j) {
+					return;
+				}
+				
+			}
+			
+			
+		}
+		
+		if(j==N-1) {
+			num=num+1;
+			return;
+		}
+		
+		
+		until[count][0]=i;
+		until[count][1]=j;
+		
+		for(int m=0;m<N;m++) {
+			recursion(m,j+1,count+1);
+		}
+		
+		
+	}
 
 }
