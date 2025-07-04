f2d92c3d81f2eecfdbd49d2f080dc6af84eb21e7
MoonHeeKim <homoonshi@gmail.com>
2025-07-04 20:25:32 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 116 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<N; i++){
+            for (int j = N; j > 0; j--) {
+                if (j <= i) {
+                    bw.write("*");
+                } else {
+                    bw.write(" ");
+                }
+            }
+            for (int j = 1; j < i; j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        for(int i=N; i>0; i--) {
+            for (int j = 0; j < N; j++) {
+                if (j < N-i) {
+                    bw.write(" ");
+                } else {
+                    bw.write("*");
+                }
+            }
+            for (int j = 0; j < i-1; j++){
+                bw.write("*");
+            }
+            if (i != 1) {
+                bw.write("\n");
+            }
+        }
+
+        bw.flush();
+
+    }
+}
