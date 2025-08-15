4beea44dfd7ffb4db0f06c6194a9fc81679b0966
MoonHeeKim <homoonshi@gmail.com>
2025-08-16 05:44:10 +0900

    [Bronze III] Title: 학점계산, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+
+        double[] res = {4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0.0};
+        String[] score = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"};
+
+        for(int i=0; i<res.length; i++){
+            if(input.equals(score[i])){
+                bw.write(res[i]+"");
+                break;
+            }
+        }
+
+        bw.flush();
+
+    }
+}
