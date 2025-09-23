a78dad60e2640794200a232abfe8e8907c2775d1
MoonHeeKim <homoonshi@gmail.com>
2025-09-23 20:13:07 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,7 @@
+SELECT P.ID, P.NAME, P.HOST_ID
+FROM PLACES P
+WHERE P.HOST_ID IN (SELECT P2.HOST_ID
+                    FROM PLACES P2
+                    GROUP BY P2.HOST_ID
+                    HAVING COUNT(*) >= 2)
+ORDER BY P.ID
